c6b24df27432f3ac169798aedb44c2e1fd3615c0
Vitor Vianna <vitormatheusfv@gmail.com>
2023-05-10 19:48:16 -0300

    Exception handling - delete

---
+++ src/main/java/com/cursoudemy/curso/services/exceptions/DatabaseException.java
@@ -0,0 +1,9 @@
+package com.cursoudemy.curso.services.exceptions;
+
+public class DatabaseException extends RuntimeException {
+
+    public DatabaseException(String msg) {
+        super(msg);
+    }
+
+}
